55df12a19643242dff24f8a0d0a582e0ae9d04de
Samuel Li <samuelili@users.noreply.github.com>
2019-09-30 22:19:54 -0700

    Fixed accidental typo

--- OffSeasonBot2019/src/main/java/com/team2813/frc2019/subsystems/SubsystemControlsConfig.java
+++ OffSeasonBot2019/src/main/java/com/team2813/frc2019/subsystems/SubsystemControlsConfig.java
@@ -1,11 +1,9 @@
-packaggit stae com.team2813.frc2019.subsystems;
+package com.team2813.frc2019.subsystems;
 
 import edu.wpi.first.wpilibj.Joystick;
 
 class SubsystemControlsConfig {
 
 	static Joystick driveJoystick = new Joystick(0);
-
-
 
 }
