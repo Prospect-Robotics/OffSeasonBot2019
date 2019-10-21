f6ea469aeee56aedcc67e5d55cde1cd3e25fd501
David Daney <david.s.daney@gmail.com>
2019-10-20 22:33:00 -0700

    Change to SparkMax 1.4.1

--- OffSeasonBot2019/src/main/java/com/team2813/lib/sparkMax/SparkMaxException.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/sparkMax/SparkMaxException.java
@@ -1,12 +1,10 @@
 package com.team2813.lib.sparkMax;
 
 import com.revrobotics.CANError;
-import com.revrobotics.CANSparkMaxLowLevel.ParameterStatus;
 
 public class SparkMaxException extends Exception {
 
 	CANError canError;
-	ParameterStatus parameterStatus;
 	String message;
 
 	public SparkMaxException(CANError canError, String message) {
@@ -19,15 +17,7 @@ public class SparkMaxException extends Exception {
 		this(canError, canError.toString());
 	}
 
-	public SparkMaxException(ParameterStatus parameterStatus, String message) {
-		super();
-		this.parameterStatus = parameterStatus;
-		this.message = message;
-	}
 
-	public SparkMaxException(ParameterStatus parameterStatus) {
-		this(parameterStatus, parameterStatus.toString());
-	}
 
 	public SparkMaxException(String message, Throwable cause) {
 		super(message, cause);
@@ -36,31 +26,19 @@ public class SparkMaxException extends Exception {
 	public SparkMaxException(String message, SparkMaxException cause) {
 		super(message, cause);
 		canError = cause.canError;
-		parameterStatus = cause.parameterStatus;
 	}
 
 	public static void throwIfNotOk(String message, CANError canError) throws SparkMaxException {
-		if (canError != CANError.kOK) {
+		if (canError != CANError.kOk) {
 			throw new SparkMaxException(canError, message);
 		}
 	}
 
 	public static void throwIfNotOk(CANError canError) throws SparkMaxException {
-		if (canError != CANError.kOK) {
+		if (canError != CANError.kOk) {
 			throw new SparkMaxException(canError);
 		}
 	}
 
-	public static void throwIfNotOk(String message, ParameterStatus parameterStatus) throws SparkMaxException {
-		if (parameterStatus != ParameterStatus.kOK) {
-			throw new SparkMaxException(parameterStatus, message);
-		}
-	}
-
-	public static void throwIfNotOk(ParameterStatus parameterStatus) throws SparkMaxException {
-		if (parameterStatus != ParameterStatus.kOK) {
-			throw new SparkMaxException(parameterStatus);
-		}
-	}
 
 }
