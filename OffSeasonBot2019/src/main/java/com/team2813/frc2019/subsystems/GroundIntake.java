1586b97caa4af88083b034db1f57ef9384f90719
Grady Whelan <grady.whelan@gmail.com>
2019-11-06 14:43:28 -0800

    Update GroundIntake.java - not functional

--- OffSeasonBot2019/src/main/java/com/team2813/frc2019/subsystems/GroundIntake.java
+++ OffSeasonBot2019/src/main/java/com/team2813/frc2019/subsystems/GroundIntake.java
@@ -1,15 +1,22 @@
 package com.team2813.frc2019.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
 import com.team2813.lib.controls.Button;
+import com.team2813.lib.sparkMax.CANSparkMaxWrapper;
 import com.team2813.lib.sparkMax.SparkMaxException;
 import com.team2813.lib.talon.CTREException;
 import com.team2813.lib.talon.VictorWrapper;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class GroundIntake extends Subsystem1d<GroundIntake.Position> {
+public class GroundIntake extends Subsystem {
 
-    private Position currentPosition = Position.RETRACTED;
+    private Position targetPosition = Position.RETRACTED;
+    private double encoderPosition;
+    private final double ALLOWABLE_ERROR = 0.15;
+    private final double PERCENT = 0.5;
+    private final CANSparkMaxWrapper MOTOR = SubsystemMotorConfig.groundIntakeArm;
 
     private static final Button ROLLER_OUT = SubsystemControlsConfig.groundIntakeRollerOut;
     private static final Button ROLLER_IN = SubsystemControlsConfig.groundIntakeRollerIn;
@@ -18,8 +25,8 @@ public class GroundIntake extends Subsystem1d<GroundIntake.Position> {
     private static final VictorWrapper ROLLER = SubsystemMotorConfig.groundIntakeRoller;
 	private static final double ROLLER_SPEED = 0.7;
 
-    GroundIntake() {
-        super(SubsystemMotorConfig.groundIntakeArm);
+	GroundIntake() {
+	    
     }
 
     @Override
@@ -28,13 +35,28 @@ public class GroundIntake extends Subsystem1d<GroundIntake.Position> {
     }
 
     @Override
-    protected void outputTelemetry_() throws CTREException, SparkMaxException {
-		SmartDashboard.putNumber("GroundIntake Demand", periodicIO.demand);
-		SmartDashboard.putNumber("GroundIntake Position", periodicIO.positionTicks);
+    protected void outputTelemetry_() {
+        SmartDashboard.putNumber("MainIntake Target", targetPosition.getPos());
+        SmartDashboard.putNumber("MainIntake Position", encoderPosition);
     }
 
     @Override
-    protected void teleopControls_() throws CTREException, SparkMaxException {
+    protected void writePeriodicOutputs_() throws SparkMaxException {
+        if (targetPosition.getPos() - encoderPosition > ALLOWABLE_ERROR) {
+            MOTOR.set(PERCENT, ControlType.kDutyCycle);
+        } else {
+            MOTOR.setNeutralMode(CANSparkMax.IdleMode.kBrake);
+            MOTOR.set(0.0, ControlType.kDutyCycle);
+        }
+    }
+
+    @Override
+    protected void readPeriodicInputs_() {
+        encoderPosition = MOTOR.getEncoderPosition();
+    }
+
+    @Override
+    protected void teleopControls_() throws CTREException {
         TOGGLE_POSITION.whenPressed(this::togglePosition);
         ROLLER_OUT.whenPressed(() -> {
             try {
@@ -70,24 +92,13 @@ public class GroundIntake extends Subsystem1d<GroundIntake.Position> {
 
     }
 
+    private void togglePosition() {
+        targetPosition = targetPosition.getNextClockwise();
+    }
+
     private void setRollerOut(boolean out) throws CTREException {
         double percent = out ? ROLLER_SPEED : -ROLLER_SPEED;
         ROLLER.set(ControlMode.PercentOutput, percent);
-    }
-
-    private void togglePosition() {
-        setNextPosition(true);
-    }
-
-    @Override
-    void setNextPosition(boolean clockwise) {
-        currentPosition = currentPosition.getClock(clockwise);
-        setPosition(currentPosition);
-    }
-
-    void setNextPosition(Position position) {
-        currentPosition = position;
-        setPosition(position);
     }
 
     enum Position implements Subsystem1d.Position<GroundIntake.Position> {
@@ -112,7 +123,7 @@ public class GroundIntake extends Subsystem1d<GroundIntake.Position> {
             public Position getNextCounter() {
                 return RETRACTED;
             }
-        };
+        }, LIMIT (3.0); // TODO: 11/05/2019 find correct value
 
         private final double position;
 
