250e41f8c68ab6722b845ed85add49069575a3aa
abhineetpal <aip_embox@thepal.org>
2019-12-06 23:35:41 -0800

    Update PurePursuit.java
    was finally able to make the method return the location

--- OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
@@ -1,15 +1,24 @@
 package com.team2813.lib.purePursuit;
 
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+import static java.awt.geom.Point2D.*;
+
 public class PurePursuit {
-    public void getStarterValue(double rightEncoderVal, double leftEncoderVal){
+    public ArrayList<Point2D> getLocation(double rightEncoderVal, double leftEncoderVal, ArrayList<Point2D> coordinates) {
         double currentRightVal = 0.0;
         double currentLeftVal = 0.0;
         double robot_angle = 0.0; //radians
+        Point2D location = new Point2D.Double();
         double deltaRightVal = currentRightVal - rightEncoderVal;
         double deltaLeftVal = currentLeftVal - leftEncoderVal;
-        double dist = (deltaRightVal + deltaLeftVal)/2;
+        double dist = (deltaRightVal + deltaLeftVal) / 2;
         double xVal = dist * Math.cos(robot_angle);
         double yVal = dist * Math.sin(robot_angle);
+        location.setLocation(xVal, yVal);
+        coordinates.add(location);
+        return coordinates;
     }
 }
