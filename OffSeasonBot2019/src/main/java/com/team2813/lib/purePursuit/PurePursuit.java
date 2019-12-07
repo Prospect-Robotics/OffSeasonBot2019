31193149a47237a29dd0ddc77cc4661f69d32122
abhineetpal <aip_embox@thepal.org>
2019-12-06 21:27:32 -0800

    Update PurePursuit.java
    did most of the function to get the start location. just need to find out how to return x and y. also got rid of endPosition parameter, because was not needed in calculations of start location

--- OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
@@ -2,7 +2,14 @@ package com.team2813.lib.purePursuit;
 
 
 public class PurePursuit {
-    public void getStarterValue(double rightEncoderVal, double leftEncoderVal, double endPosition){
-
+    public void getStarterValue(double rightEncoderVal, double leftEncoderVal){
+        double currentRightVal = 0.0;
+        double currentLeftVal = 0.0;
+        double robot_angle = 0.0; //radians
+        double deltaRightVal = currentRightVal - rightEncoderVal;
+        double deltaLeftVal = currentLeftVal - leftEncoderVal;
+        double dist = (deltaRightVal + deltaLeftVal)/2;
+        double xVal = dist * Math.cos(robot_angle);
+        double yVal = dist * Math.sin(robot_angle);
     }
 }
