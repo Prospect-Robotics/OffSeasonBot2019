0a9d09f13f416c022574fa78dd64602918c1f63a
saanvi25 <55930332+saanvi25@users.noreply.github.com>
2019-12-06 16:25:09 -0800

    Create purePursuit.java

---
+++ OffSeasonBot2019/src/main/java/com/team2813/frc2019/subsystems/purePursuit.java
@@ -0,0 +1,8 @@
+package com.team2813.frc2019.subsystems;
+
+
+public class purePursuit {
+    public void getStarterValue(double rightEncoderVal, double leftEncoderVal, double endPosition){
+
+    }
+}
