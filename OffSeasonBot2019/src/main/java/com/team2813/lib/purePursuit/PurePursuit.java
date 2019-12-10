e840c070d045d3bd34d1e8c4b4fc10594da26f9c
abhineetpal <aip_embox@thepal.org>
2019-12-09 16:18:46 -0800

    Update PurePursuit.java
    finished point injecter

--- OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/purePursuit/PurePursuit.java
@@ -22,14 +22,29 @@ public class PurePursuit {
         return location;
     }
     //injects points
-    public ArrayList<Point2D> injectPoints(ArrayList<Point2D> path, double spacing){
+    public ArrayList<Point2D> injectPoints(ArrayList<Point2D> path, double spacing) {
+        int numSegments = path.size() - 1;
         for(int i = 0; i < path.size(); i++){
-            Point2D startPosition = path.get(i);
-            Point2D endPosition = path.get(i + 1);
-            double xDif = endPosition.getX() - startPosition.getX();
-            double yDif = endPosition.getY() - startPosition.getY();
-            Point2D vector = new Point2D.Double();
-            vector.setLocation(xDif, yDif);
+            if(numSegments != 0){
+                Point2D startPosition = path.get(i);
+                Point2D endPosition = path.get(i + 1);
+                double xDif = endPosition.getX() - startPosition.getX();
+                double yDif = endPosition.getY() - startPosition.getY();
+                Point2D vector = new Point2D.Double();
+                vector.setLocation(xDif, yDif);
+                double vectorMagnitude = Math.sqrt((vector.getX() * vector.getX()) + (vector.getY() * vector.getY()));
+                double numPoints = Math.ceil(Math.sqrt(vectorMagnitude/spacing));
+                Point2D unitVector = new Point2D.Double();
+                unitVector.setLocation(xDif/vectorMagnitude * spacing, yDif/vectorMagnitude * spacing);
+                Point2D newPoint = new Point2D.Double();
+                for(int j = 0; j < numPoints; j++){
+                    newPoint.setLocation(startPosition.getX() + unitVector.getX() * j, startPosition.getY() + unitVector.getY() * j);
+                    path.add(i + j + 1, newPoint);
+                }
+                i += numPoints;
+                numSegments -= 1;
+            }
         }
+        return path;
     }
 }
