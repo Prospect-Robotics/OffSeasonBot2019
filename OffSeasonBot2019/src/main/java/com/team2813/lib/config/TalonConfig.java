7b2db61ec7004cd374584bec08b2d0760ff493a1
andrew-sheu <55634584+andrew-sheu@users.noreply.github.com>
2019-12-11 15:31:06 -0800

    Update TalonConfig.java
    I added all the methods to Talon Config.

--- OffSeasonBot2019/src/main/java/com/team2813/lib/config/TalonConfig.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/config/TalonConfig.java
@@ -1,4 +1,145 @@
 package com.team2813.lib.config;
 
+import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
+import com.ctre.phoenix.motorcontrol.VelocityMeasPeriod;
+import com.team2813.lib.talon.BaseMotorControllerWrapper;
+
 public class TalonConfig extends MotorConfig {
+    @Override
+    public int getDeviceNumber() {
+        return deviceNumber;
+    }
+
+    @Override
+    public void setDeviceNumber(int deviceNumber) {
+        this.deviceNumber = deviceNumber;
+    }
+
+    public int getPeakCurrentDuration() {
+        return peakCurrentDuration;
+    }
+
+    public void setPeakCurrentDuration(int peakCurrentDuration) {
+        this.peakCurrentDuration = peakCurrentDuration;
+    }
+
+    public int getPeakCurrentLimit() {
+        return peakCurrentLimit;
+    }
+
+    public void setPeakCurrentLimit(int peakCurrentLimit) {
+        this.peakCurrentLimit = peakCurrentLimit;
+    }
+
+    public boolean isEnableVoltageCompensation() {
+        return enableVoltageCompensation;
+    }
+
+    public void setEnableVoltageCompensation(boolean enableVoltageCompensation) {
+        this.enableVoltageCompensation = enableVoltageCompensation;
+    }
+
+    public int getCompSaturationVoltage() {
+        return compSaturationVoltage;
+    }
+
+    public void setCompSaturationVoltage(int compSaturationVoltage) {
+        this.compSaturationVoltage = compSaturationVoltage;
+    }
+
+    public int getContinuousCurrentLimitAmps() {
+        return continuousCurrentLimitAmps;
+    }
+
+    public void setContinuousCurrentLimitAmps(int continuousCurrentLimitAmps) {
+        this.continuousCurrentLimitAmps = continuousCurrentLimitAmps;
+    }
+
+    public int getMotionAcceleration() {
+        return motionAcceleration;
+    }
+
+    public void setMotionAcceleration(int motionAcceleration) {
+        this.motionAcceleration = motionAcceleration;
+    }
+
+    public int getMotionCruiseVelocity() {
+        return motionCruiseVelocity;
+    }
+
+    public void setMotionCruiseVelocity(int motionCruiseVelocity) {
+        this.motionCruiseVelocity = motionCruiseVelocity;
+    }
+
+    public double getClosedLoopRampRate() {
+        return closedLoopRampRate;
+    }
+
+    public void setClosedLoopRampRate(double closedLoopRampRate) {
+        this.closedLoopRampRate = closedLoopRampRate;
+    }
+
+    public double getOpenLoopRampRate() {
+        return openLoopRampRate;
+    }
+
+    public void setOpenLoopRampRate(double openLoopRampRate) {
+        this.openLoopRampRate = openLoopRampRate;
+    }
+
+    public boolean isInvertSensorPhase() {
+        return invertSensorPhase;
+    }
+
+    public void setInvertSensorPhase(boolean invertSensorPhase) {
+        this.invertSensorPhase = invertSensorPhase;
+    }
+
+    public StatusFrameEnhanced getStatusFrame() {
+        return statusFrame;
+    }
+
+    public void setStatusFrame(StatusFrameEnhanced statusFrame) {
+        this.statusFrame = statusFrame;
+    }
+
+    public int getStatusFramePeriod() {
+        return statusFramePeriod;
+    }
+
+    public void setStatusFramePeriod(int statusFramePeriod) {
+        this.statusFramePeriod = statusFramePeriod;
+    }
+
+    public VelocityMeasPeriod getVelocityMeasurementPeriod() {
+        return velocityMeasurementPeriod;
+    }
+
+    public void setVelocityMeasurementPeriod(VelocityMeasPeriod velocityMeasurementPeriod) {
+        this.velocityMeasurementPeriod = velocityMeasurementPeriod;
+    }
+
+    public BaseMotorControllerWrapper.VelocityMeasurementWindow getVelocityMeasurementWindow() {
+        return velocityMeasurementWindow;
+    }
+
+    public void setVelocityMeasurementWindow(BaseMotorControllerWrapper.VelocityMeasurementWindow velocityMeasurementWindow) {
+        this.velocityMeasurementWindow = velocityMeasurementWindow;
+    }
+
+    private int deviceNumber;
+    private int peakCurrentDuration;
+    private int peakCurrentLimit;
+    private boolean enableVoltageCompensation;
+    private int compSaturationVoltage;
+    private int continuousCurrentLimitAmps;
+    private int motionAcceleration;
+    private int motionCruiseVelocity;
+    private double closedLoopRampRate;
+    private double openLoopRampRate;
+    private boolean invertSensorPhase;
+    private StatusFrameEnhanced statusFrame; // cannot serialize into PeriodicFrame (see getStatusFrame)
+    private int statusFramePeriod = 5;
+    private VelocityMeasPeriod velocityMeasurementPeriod;
+    private BaseMotorControllerWrapper.VelocityMeasurementWindow velocityMeasurementWindow;
 }
