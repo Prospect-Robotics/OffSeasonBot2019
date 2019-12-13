cb17c7855005eaa3abcbec8323fa78fedd2cd1ec
Grady Whelan <grady.whelan@gmail.com>
2019-12-12 21:33:44 -0800

    cleanup/remove old annotation stuff

--- OffSeasonBot2019/src/main/java/com/team2813/lib/talon/options/LimitDirection.java
+++
@@ -1,9 +0,0 @@
-package com.team2813.lib.talon.options;
-
-/**
- * @author Grady Whelan
- * Forward or reverse direction of Limits on motor controller
- */
-public enum LimitDirection {
-    FORWARD, REVERSE
-}
