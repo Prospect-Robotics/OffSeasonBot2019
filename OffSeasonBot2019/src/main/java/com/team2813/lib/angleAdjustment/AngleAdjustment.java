d7239753cb9004dfb6b7c36797cdc54e7bd43091
btepegoz <beyzanurtepegoz@gmail.com>
2019-12-13 15:50:16 -0800

    ...clean uup code, may contain errors

--- OffSeasonBot2019/src/main/java/com/team2813/lib/angleAdjustment/AngleAdjustment.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/angleAdjustment/AngleAdjustment.java
@@ -6,19 +6,23 @@ public class AngleAdjustment {
     private double initialLeft;
     private double finalLeft;
     private double angleChange;
-    private final double GEAR_RATIO = 1; //TODO ask Kyle for Gear Ratio
+    private final double GEAR_RATIO = 9/60; // ask Kyle for Gear Ratio
     private final double WHEEL_DIAMETER = 4;
-    private final double WHEEL_CIRCUMFERENCE = 4 * Math.PI;
-
-    public AngleAdjustment(double initialRight, double initialLeft, double angleChange){
+    private final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+    private final int PULSES;
+    private final int PULSES_PER_REVOLUTION;
+    //TODO add a parameter, either WHEEL_DIAMETER or WHEEL_Circumference for later use
+    public AngleAdjustment(double initialRight, double initialLeft, double angleChange, int pulses, int pulsesPerRevolution){
         this.initialRight = initialRight;
         this.initialLeft = initialLeft;
         this.angleChange = angleChange;
+        this.PULSES_PER_REVOLUTION = pulsesPerRevolution;
+        this.PULSES = pulses;
         calculate();
-        //TODO pulse / pulse per rev * 360 % 360
+        //FIX pulse / pulse per rev * 360 % 360
+        this.angleChange = PULSES/PULSES_PER_REVOLUTION*360%360;
         // 42 counts per rev
         //TODO convert degrees to encoder values which the left and right must travel
-
     }
 
     public double getFinalRight() {
@@ -29,7 +33,7 @@ public class AngleAdjustment {
         return finalLeft;
     }
 
-    private double calculate() {
+    private void calculate() {
         double counts = angleChange / 360 * 42;
         double wheelDegree = counts * GEAR_RATIO;
         this.finalRight = initialRight + wheelDegree;
