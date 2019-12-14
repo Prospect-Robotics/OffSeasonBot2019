cd31a850999116b2e74e42ad956c2bdc2628882f
Gear Heads Lab Computer <github@team2813.com>
2019-12-13 16:32:32 -0800

    Fixed a few errors, added fields and parameters

--- OffSeasonBot2019/src/main/java/com/team2813/lib/angleAdjustment/AngleAdjustment.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/angleAdjustment/AngleAdjustment.java
@@ -6,21 +6,20 @@ public class AngleAdjustment {
     private double initialLeft;
     private double finalLeft;
     private double angleChange;
-    private final double GEAR_RATIO = 9/60; // ask Kyle for Gear Ratio
-    private final double WHEEL_DIAMETER = 4;
-    private final double WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
-    private final int PULSES;
+    private final double GEAR_RATIO;
+    private final double WHEEL_DIAMETER;
+    private final double WHEEL_CIRCUMFERENCE;
     private final int PULSES_PER_REVOLUTION;
-    //TODO add a parameter, either WHEEL_DIAMETER or WHEEL_Circumference for later use
-    public AngleAdjustment(double initialRight, double initialLeft, double angleChange, int pulses, int pulsesPerRevolution){
+    //TODO add a parameter, either WHEEL_DIAMETER
+    public AngleAdjustment(double initialRight, double initialLeft, double angleChange,  int pulsesPerRevolution, double wheelDiameter, double gearRatio){
         this.initialRight = initialRight;
         this.initialLeft = initialLeft;
         this.angleChange = angleChange;
-        this.PULSES_PER_REVOLUTION = pulsesPerRevolution;
-        this.PULSES = pulses;
+        PULSES_PER_REVOLUTION = pulsesPerRevolution;
+        WHEEL_DIAMETER = wheelDiameter;
+        WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI;
+        GEAR_RATIO = gearRatio;
         calculate();
-        //FIX pulse / pulse per rev * 360 % 360
-        this.angleChange = PULSES/PULSES_PER_REVOLUTION*360%360;
         // 42 counts per rev
         //TODO convert degrees to encoder values which the left and right must travel
     }
