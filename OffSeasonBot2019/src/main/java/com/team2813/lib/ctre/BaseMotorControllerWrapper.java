852bcc501c4ef944407756ac9dd50897412819a3
Grady Whelan <grady.whelan@gmail.com>
2019-12-28 15:42:48 -0800

    added more to pigeon wrapper

--- OffSeasonBot2019/src/main/java/com/team2813/lib/ctre/BaseMotorControllerWrapper.java
+++ OffSeasonBot2019/src/main/java/com/team2813/lib/ctre/BaseMotorControllerWrapper.java
@@ -791,24 +791,6 @@ public abstract class BaseMotorControllerWrapper<Controller extends BaseMotorCon
 	}
 
 	/**
-	 * Enum storing different timeout values in ms for construction time
-	 * or runtime updates.
-	 */
-	public enum TimeoutMode {
-		/** Longer timeout, used for constructors */
-		CONSTRUCTING(100),
-		/** Shorter timeout, used for on the fly updates */
-		RUNNING(20),
-		NO_TIMEOUT(0);
-
-		final int valueMs;
-
-		private TimeoutMode(int valueMs) {
-			this.valueMs = valueMs;
-		}
-	}
-
-	/**
 	 * PID ID Slots: Primary 0; Auxiliary 1
 	 */
 	public enum PidIdx { //FIXME give this a name that reflects w/e it actually does
